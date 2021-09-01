5d5679e88fe6d834a4585d1fb7ea11c2c16b7748
pagrub <pagrub@ttu.ee>
2021-09-01 20:10:17 +0300

    iteration fibbonachi

--- AL01A/src/ee/ttu/algoritmid/fibonacci/AL01A.java
+++ AL01A/src/ee/ttu/algoritmid/fibonacci/AL01A.java
@@ -1,16 +1,16 @@
+/**
+ * Package for fibonacci sequence iteratively.
+ */
 package ee.ttu.algoritmid.fibonacci;
 
 public class AL01A {
-
-    private int n;
 
     /**
      * Compute the Fibonacci sequence numbr.
      * @param n The number of the sequence to compute.
      * @return The n-th number in Fibonacci series.
      */
-    public String iterativeF(int n) {
-        this.n = n;
+    public String iterativeF(final Integer n) {
         Integer[] f = new Integer[n + 1];
         f[0] = f[1] = 1;
         for (int i = 2; i <= n; i++) {
