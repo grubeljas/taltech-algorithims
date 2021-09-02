d6cbbce34e9e96d7cc0729165693ad18d0c3f86b
Pavel <pagrub@ttu.ee>
2021-09-02 14:39:29 +0300

    р

--- AL01B/src/ee/ttu/algoritmid/fibonacci/AL01B.java
+++ AL01B/src/ee/ttu/algoritmid/fibonacci/AL01B.java
@@ -30,16 +30,12 @@ public class AL01B {
      * @param n The number of the sequence to compute.
      * @return The n-th number in Fibonacci series.
      */
-    public String iterativeF(final int n) {
-        BigInteger a = new BigInteger("0");
-        BigInteger b = new BigInteger("1");
-        BigInteger c;
-        for (int i = 1; i <= n; i++) {
-            c = a.add(b);
-            a = b;
-            b = c;
-        }
-        return String.valueOf(a);
+    public String iterativeF(int n) {
+        Integer[] f = new Integer[n + 1];
+        f[0] = f[1] = 1;
+        for (int i = 2; i <= n; i++)
+            f[i] = f[i-1] + f[i-2];
+        return String.valueOf(f[n]);
     }
 
     /**
