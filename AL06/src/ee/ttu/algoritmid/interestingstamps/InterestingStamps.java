91b88e94a27c0f25f2d4fcfd6247d82cd940067b
pagrub <pagrub@ttu.ee>
2021-11-29 04:48:55 +0200

    recursive fibbonachi

--- AL06/src/ee/ttu/algoritmid/interestingstamps/InterestingStamps.java
+++ AL06/src/ee/ttu/algoritmid/interestingstamps/InterestingStamps.java
@@ -7,21 +7,27 @@ import java.util.stream.Collectors;
 public class InterestingStamps {
 
     public static List<Integer> findStamps(int sum, List<Integer> stampOptions) throws IllegalArgumentException {
-        stampOptions = stampOptions.stream().sorted().collect(Collectors.toList());
+
         List<Integer>[] seq = new LinkedList[sum+1];
 
-        for (int i = 0; i <= sum; i += stampOptions.stream().mapToInt(c -> c).min().orElseThrow()) {
-            List<Integer> variant;
+        for (int i = 0; i < sum + 1; i++) {
+            seq[i] = new LinkedList<Integer>();
+        }
+
+        for (int i = 0; i <= sum; i++) {
             seq[i] = new LinkedList<Integer>();
             for (int j = 0; j < stampOptions.size(); j++){
                 if (stampOptions.get(j) <= i) {
-                    variant = seq[i - stampOptions.get(j)];
-                    variant.add(stampOptions.get(j));
-                    if (seq[i].size() >= variant.size() && sum(variant) <= sum
-                    && sum(variant) - sum(seq[i]) > 0) {
-                        seq[i] = getValues(seq[i], variant);
+                    List<Integer> variant = new LinkedList<>();
+                    if (i != 0) {
+                        seq[i] = max(seq[i], seq[i - 1]);
                     }
+                    if (sum(seq[i - stampOptions.get(j)]) + stampOptions.get(j) <= i) {
+                        variant = new LinkedList<>(seq[i - stampOptions.get(j)]);
+                    }
+                    variant.add(stampOptions.get(j));
                     seq[i] = max(seq[i], variant);
+                    System.out.println(Arrays.toString(seq));
                 }
             }
         }
@@ -30,16 +36,19 @@ public class InterestingStamps {
     }
 
     private static int sum(List<Integer> stampOptions) {
+        if (stampOptions.isEmpty()) {
+            return 0;
+        }
         return stampOptions.stream().mapToInt(Integer::intValue).sum();
     }
 
     private static List<Integer> getValues(List<Integer> stampOptions1, List<Integer> stampOptions2) {
         int h1 = 0, h2 = 0;
         for (Integer integer: stampOptions1) {
-            h1 += ((integer % 10 == 0 || integer == 1) ? 1 : 0);
+            h1 += ((integer % 10 == 0 || integer == 1) ? 0 : 1);
         }
         for (Integer integer: stampOptions2) {
-            h2 += ((integer % 10 == 0 || integer == 1) ? 1 : 0);
+            h2 += ((integer % 10 == 0 || integer == 1) ? 0 : 1);
         }
         return (h1 < h2) ? stampOptions2 : stampOptions1;
     }
@@ -51,13 +60,16 @@ public class InterestingStamps {
 
     private static List<Integer> max(List<Integer> i, List<Integer> j)
     {
-        return (i.size() > j.size()) ? i : j;
+        if (sum(i) == sum(j) && i.size() == j.size()) {
+            return getValues(i, j);
+        }
+        return (sum(i) > sum(j) && i.size() <= j.size()) ? i : j;
     }
 
     public static void main(String[] args)
     {
-        int W = 5;
-        List<Integer> val = List.of(1, 2, 3);
+        int W = 100;
+        List<Integer> val = List.of(1, 10, 24, 30, 33, 36);
         System.out.println(findStamps(W, val));
     }
 
