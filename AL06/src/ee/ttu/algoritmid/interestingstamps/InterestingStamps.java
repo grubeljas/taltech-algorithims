183f365bf2deed92991ab83866a010947a4201f0
pagrub <pagrub@ttu.ee>
2021-11-29 02:12:52 +0200

    recursive fibbonachi

--- AL06/src/ee/ttu/algoritmid/interestingstamps/InterestingStamps.java
+++ AL06/src/ee/ttu/algoritmid/interestingstamps/InterestingStamps.java
@@ -1,6 +1,5 @@
 package ee.ttu.algoritmid.interestingstamps;
-import javax.swing.*;
-import java.util.Comparator;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -8,57 +7,51 @@ import java.util.stream.Collectors;
 public class InterestingStamps {
 
     public static List<Integer> findStamps(int sum, List<Integer> stampOptions) throws IllegalArgumentException {
-        int len = stampOptions.size();
         stampOptions = stampOptions.stream().sorted().collect(Collectors.toList());
-        List<Integer> stampValues = getValues(stampOptions);
-        int max, current = 0;
-        int v[] = new int[sum + 1]; //map of value of collection
-        int s[] = new int[sum + 1]; //map of sum of collection
-        int size[] = new int[sum + 1]; //map of size of collections
-        LinkedList<Integer> seq[] = new LinkedList[sum + 1];
+        List<Integer>[] seq = new LinkedList[sum+1];
 
         for (int i = 0; i <= sum; i += stampOptions.stream().mapToInt(c -> c).min().orElseThrow()) {
-            LinkedList<Integer> linkedList = new LinkedList<>();
+            List<Integer> variant;
+            seq[i] = new LinkedList<Integer>();
             for (int j = 0; j < stampOptions.size(); j++){
                 if (stampOptions.get(j) <= i) {
-                    if (i == 100) {
-                        System.out.println(s);
+                    variant = seq[i - stampOptions.get(j)];
+                    variant.add(stampOptions.get(j));
+                    if (seq[i].size() >= variant.size() && sum(variant) <= sum
+                    && sum(variant) - sum(seq[i]) > 0) {
+                        seq[i] = getValues(seq[i], variant);
                     }
-                    if (s[i] <= s[i - stampOptions.get(j)] + stampOptions.get(j) &&
-                            s[i - stampOptions.get(j)] + stampOptions.get(j) <= i) {
-
-                        s[i] = s[i - stampOptions.get(j)] + stampOptions.get(j); //МБ ПЕРЕДВИНУТЬ ВНИЗ
-                        if (size[i] >= size[i - stampOptions.get(j)] + 1) {
-                            if (v[i] < v[i - stampOptions.get(j)] + stampValues.get(j)) {
-                                s[i] = s[i - stampOptions.get(j)] + stampOptions.get(j);
-                                size[i] = size[i - stampOptions.get(j)] + 1;
-                                v[i] = v[i - stampOptions.get(j)] + stampValues.get(j);
-                                seq[i].add(stampOptions.get(j));
-                            }
-                        }
-
-                    }
-
-
-                    s[i] = max(s[i], s[i - stampOptions.get(j)] + stampOptions.get(j));
-                    v[i] = max(v[i], v[i - stampOptions.get(j)] + stampValues.get(j));
+                    seq[i] = max(seq[i], variant);
                 }
             }
         }
+        System.out.println(Arrays.toString(seq));
         return seq[sum];
     }
 
-    private static List<Integer> getValues(List<Integer> stampOptions) {
-        List<Integer> integerList = new LinkedList<>();
-        for (Integer integer: stampOptions) {
-            integerList.add((integer % 10 == 0 || integer == 1) ? 1 : 0);
+    private static int sum(List<Integer> stampOptions) {
+        return stampOptions.stream().mapToInt(Integer::intValue).sum();
+    }
+
+    private static List<Integer> getValues(List<Integer> stampOptions1, List<Integer> stampOptions2) {
+        int h1 = 0, h2 = 0;
+        for (Integer integer: stampOptions1) {
+            h1 += ((integer % 10 == 0 || integer == 1) ? 1 : 0);
         }
-        return integerList;
+        for (Integer integer: stampOptions2) {
+            h2 += ((integer % 10 == 0 || integer == 1) ? 1 : 0);
+        }
+        return (h1 < h2) ? stampOptions2 : stampOptions1;
     }
 
     private static int max(int i, int j)
     {
         return (i > j) ? i : j;
+    }
+
+    private static List<Integer> max(List<Integer> i, List<Integer> j)
+    {
+        return (i.size() > j.size()) ? i : j;
     }
 
     public static void main(String[] args)
