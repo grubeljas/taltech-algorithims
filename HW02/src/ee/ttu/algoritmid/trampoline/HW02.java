2da73ad1add6954b9a6740442dfb2e297bac2514
pagrub <pagrub@ttu.ee>
2021-12-05 18:05:11 +0200

    hw02

--- HW02/src/ee/ttu/algoritmid/trampoline/HW02.java
+++ HW02/src/ee/ttu/algoritmid/trampoline/HW02.java
@@ -1,8 +1,6 @@
 package ee.ttu.algoritmid.trampoline;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.function.Predicate;
 
 public class HW02 implements TrampolineCenter {
 
@@ -15,55 +13,35 @@ public class HW02 implements TrampolineCenter {
         int[] coordinats = new int[]{0, 0};
         max = new int[]{fMap.length - 1, fMap[0].length - 1};
         ResultSimple result = new ResultSimple();
-        return recJumpWithWalls(coordinats, result);
+        if (hasWalls()) {
+            return recJumpWithWalls(coordinats, result);
+        } else {
+            return recJumpSimple(coordinats, result);
+        }
     }
 
-    /**
-     * Jump algorith with no wall and =.
-     * @param coordinats
-     * @param result
-     * @return
-     */
     public Result recJumpSimple(int[] coordinats, ResultSimple result) {
         if (max[0] == coordinats[0] && max[1] == coordinats[1]) {
             return result;
         }
-        System.out.println(result.getJumps());
 
         int jumpForce = fMap[coordinats[0]][coordinats[1]].getJumpForce();
         int[] southCoordinats, eastCoordinats;
         Result east = new ResultSimple(), south = new ResultSimple();
-        int jump;
 
         if (coordinats[0] + jumpForce <= max[0]) {
             //South
-            jump = jumpForce;
-            for (int i = 1; i < jumpForce; i++) {
-                if (fMap[coordinats[0] + i][coordinats[1]].getType().equals(Trampoline.Type.WALL) ||
-                        coordinats[0] + i > max[0]) { //мб разделить условия на два ифа
-                    jump = i - 1;
-                    break;
-                }
-            }
-            southCoordinats = new int[]{coordinats[0] + jump, coordinats[1]};
+            southCoordinats = new int[]{coordinats[0] + jumpForce, coordinats[1]};
             ResultSimple southResult = new ResultSimple();
             southResult.setJumps(new ArrayList<>(result.getJumps().subList(0, result.getJumps().size())));
-            southResult.addJumps("S" + jump);
+            southResult.addJumps("S" + jumpForce);
             south = recJumpSimple(southCoordinats, southResult);
         } else if (coordinats[1] + jumpForce <= max[1]) {
             //East
-            jump = jumpForce;
-            for (int i = 1; i < jumpForce; i++) {
-                if (fMap[coordinats[0]][coordinats[1] + i].getType().equals(Trampoline.Type.WALL) ||
-                        coordinats[1] + i > max[1]) { //мб разделить условия на два ифа
-                    jump = i - 1;
-                    break;
-                }
-            }
-            eastCoordinats = new int[]{coordinats[0], coordinats[1] + jump};
+            eastCoordinats = new int[]{coordinats[0], coordinats[1] + jumpForce};
             ResultSimple eastResult = new ResultSimple();
             eastResult.setJumps(new ArrayList<>(result.getJumps().subList(0, result.getJumps().size())));
-            eastResult.addJumps("E" + jump);
+            eastResult.addJumps("E" + jumpForce);
             east = recJumpSimple(eastCoordinats, eastResult);
         }
 
@@ -75,6 +53,13 @@ public class HW02 implements TrampolineCenter {
         return (south.getJumps().size() < east.getJumps().size()) ? south : east;
     }
 
+
+    /**
+     * Jump algorith with no wall and =.
+     * @param coordinats
+     * @param result
+     * @return
+     */
     public Result recJumpWithWalls(int[] coordinats, ResultSimple result) {
         System.out.println(result.getJumps());
         if (max[0] == coordinats[0] && max[1] == coordinats[1]) {
@@ -142,12 +127,13 @@ public class HW02 implements TrampolineCenter {
     }
 
     public boolean hasWalls() {
-        for (Trampoline[] trampoline: fMap) {
-            if (Arrays.stream(trampoline).anyMatch(trampoline1 -> trampoline1.getType().equals(Trampoline.Type.WALL))) {
-                return true;
+        for (Trampoline[] trampolineArray: fMap) {
+            for (Trampoline trampoline: trampolineArray) {
+                if (trampoline.getType().equals(Trampoline.Type.WALL)) {
+                    return true;
+                }
             }
         }
         return false;
     }
-
 }
