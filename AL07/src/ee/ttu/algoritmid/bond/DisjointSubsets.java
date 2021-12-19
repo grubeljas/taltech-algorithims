1fb198c7bb432bb11d7a4ed4ea8480ef56febfda
pagrub <pagrub@ttu.ee>
2021-12-19 19:26:49 +0200

    throws

--- AL07/src/ee/ttu/algoritmid/bond/DisjointSubsets.java
+++ AL07/src/ee/ttu/algoritmid/bond/DisjointSubsets.java
@@ -9,6 +9,9 @@ public class DisjointSubsets {
 
     public String find(String element) throws IllegalArgumentException {
         checkElement(element);
+        if (!parent.containsKey(element)) {
+            throw new IllegalArgumentException();
+        }
 
         if (!parent.get(element).equals(element)) {
             parent.put(element, find(parent.get(element)));
@@ -20,38 +23,29 @@ public class DisjointSubsets {
     public void union(String element1, String element2) throws IllegalArgumentException {
         checkElement(element1);
         checkElement(element2);
+        if (!parent.containsKey(element1) || !parent.containsKey(element2)) {
+            throw new IllegalArgumentException();
+        }
 
         String xRoot = find(element1), yRoot = find(element2);
-
-        // Elements are in the same set, no need
-        // to unite anything.
         if (xRoot.equals(yRoot))
             return;
-
-        // If x's rank is less than y's rank
         if (rank.get(xRoot) < rank.get(yRoot))
-
-            // Then move x under y  so that depth
-            // of tree remains less
             parent.put(xRoot, yRoot);
-
-            // Else if y's rank is less than x's rank
         else if (rank.get(yRoot) < rank.get(xRoot))
-
-            // Then move y under x so that depth of
-            // tree remains less
             parent.put(yRoot, xRoot);
-
-        else // if ranks are the same
+        else
         {
             parent.put(yRoot, xRoot);
-
             rank.put(xRoot, rank.get(xRoot) + 1);
         }
     }
 
     public void addSubset(String element) throws IllegalArgumentException {
         checkElement(element);
+        if (parent.containsKey(element)) {
+            throw new IllegalArgumentException();
+        }
         parent.put(element, element);
         rank.put(element, 1);
     }
